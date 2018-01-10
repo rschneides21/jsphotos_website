f37a47d27af9c0bf8f965b2c0ad4f47765323df1
Ryan S <ryan.schneiderman@tufts.edu>
2018-01-10 01:24:11 -0500

    settings

--- iotd/iotd/settings.py
+++ iotd/iotd/settings.py
@@ -130,5 +130,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_ROOT = os.path.join(BASE_DIR, "..", "www", "media")
 MEDIA_URL = '/media/'
 
-TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),)
+#TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),)
+TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'iotd/templates'),)
 
