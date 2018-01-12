d598ca9e262f88ec8cc4be5a79d04e6f60cfdfd9
Ryan S <ryan.schneiderman@tufts.edu>
2018-01-12 13:44:37 -0500

    fixed module error

--- iotd/iotd/urls.py
+++ iotd/iotd/urls.py
@@ -2,12 +2,13 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-import images.views
+import gallery.views
 
 #eventually have images path be image category pulled from database
 urlpatterns = [
 	#try changing include(admin.site.urls) -> admin.site.urls
     url(r'^admin/', include(admin.site.urls)),
+    #TC1 added images path, not sure about 'images.urls'
     url(r'^gallery/', include('gallery.urls')),
     url(r'^$', images.views.home, name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
