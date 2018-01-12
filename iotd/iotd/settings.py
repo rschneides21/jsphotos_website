878dfb70e288d64d5766438e240b765f1eeda45a
Ryan S <ryan.schneiderman@tufts.edu>
2018-01-12 14:17:54 -0500

    removed template debug from settings

--- iotd/iotd/settings.py
+++ iotd/iotd/settings.py
@@ -54,7 +54,6 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
-        'TEMPLATE_DEBUG': True,
         'OPTIONS': {
             'debug': DEBUG,
             'context_processors': [
