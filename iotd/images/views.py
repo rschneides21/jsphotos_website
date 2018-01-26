f5e27b44003950193e230da949ecad8a90be48fb
Ryan S <ryan.schneiderman@tufts.edu>
2018-01-26 17:11:52 -0500

    gallery.html

--- iotd/images/views.py
+++ iotd/images/views.py
@@ -9,7 +9,7 @@ def home(request):
     return render_to_response('images/home.html',
                               { 'image' : image})
 def gallery(request):
-	images = GalleryImage.objects.all()
+	images = FeaturedImage.objects.latest('uploaded')
 	# {'images' : image} == key value pair
-	return render_to_response('images/gallery.html', {'images' : images})
+	return render_to_response('images/gallery.html', {'image' : image})
 	
