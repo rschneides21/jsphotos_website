cf832d8bc9daa173a0cd3eceb4993f37892bc4cf
Ryan S <ryan.schneiderman@tufts.edu>
2018-01-26 13:49:49 -0500

    edits

--- iotd/iotd/settings.py
+++ iotd/iotd/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = '-r5cw=c2xk9!v87m2(3av2cdr!!k_4+pj&c6qc_p_o^1%m8nh^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['iotd3-dev.us-east-1.elasticbeanstalk.com', '127.0.0.1']
+ALLOWED_HOSTS = ['iotd3-dev.us-east-1.elasticbeanstalk.com']
 
 
 # Application definition
