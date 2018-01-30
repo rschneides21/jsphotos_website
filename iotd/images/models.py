237e9ca547ed413000d407735cd0c087205eedb0
Ryan S <ryan.schneiderman@tufts.edu>
2018-01-30 16:40:02 -0500

    website image

--- iotd/images/models.py
+++ iotd/images/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class FeaturedImage(models.Model):
-	
+
     name = models.CharField(max_length=200)
     tagline = models.TextField()
     uploaded = models.DateTimeField(auto_now=True)
@@ -13,4 +13,9 @@ class GalleryImage(models.Model):
 	name = models.CharField(max_length=200)
 	category = models.CharField(max_length=200)
 	uploaded = models.DateTimeField(auto_now=True)
+	img = models.ImageField(upload_to="")
+
+class WebsiteImage(models.Model):
+
+	name = models.CharField(max_length = 200)
 	img = models.ImageField(upload_to="")
