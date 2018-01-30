c4173e9d245771981a48ef36a67e1e0f9bc7bd9f
Ryan S <ryan.schneiderman@tufts.edu>
2018-01-29 23:29:18 -0500

    image array in html

--- iotd/images/views.py
+++ iotd/images/views.py
@@ -2,12 +2,6 @@ from django.shortcuts import render
 from images.models import FeaturedImage, GalleryImage
 from django.conf import settings
 from django.http import HttpResponse
-import django_tables2 as tables
-
-# right place for table code?
-class ImageTable(tables.Table):
-    class Meta:
-        model = GalleryImage 
 
 def home(request):
     image = FeaturedImage.objects.latest('uploaded') 
@@ -15,10 +9,7 @@ def home(request):
     return render(request, 'images/home.html',
                               { 'image' : image})
 def gallery(request):
-	images = GalleryImage.objects.all()
-	image_table = ImageTable(images)
-	return render(request, 'images/gallery.html', {"image_table": image_table})
-
+	images = FeaturedImage.objects.all()
 	# {'images' : image} == key value pair, json?
-	# return render(request ,'images/gallery.html', {'image' : image})
+	return render(request ,'images/gallery.html', {'images' : images})
 	
