cda60fe072d3323e1f3e53690ceaa0f4fb3fb9ea
Ryan S <ryan.schneiderman@tufts.edu>
2018-02-12 03:34:28 -0500

    changed view

--- iotd/images/views.py
+++ iotd/images/views.py
@@ -21,7 +21,7 @@ def about(request):
 	return render(request, 'images/about.html', {'images' : images, 'logos' : logos})
 
 def aimage(request, image):
-	page_image = get_object_or_404(GalleryImage, id = image)
+	page_image = GalleryImage.objects.get(name__exact = image.id)
 	logos = WebsiteImage.objects.all()
 	return render(request, 'images/image.html', {'image' : page_image, 'logos' : logos})
 
