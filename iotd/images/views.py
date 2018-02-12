f61d54a2ec277021de2949983ac92791a2bf87bc
Ryan S <ryan.schneiderman@tufts.edu>
2018-02-12 03:27:54 -0500

    maybe fixed?

--- iotd/images/views.py
+++ iotd/images/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from images.models import FeaturedImage, GalleryImage, WebsiteImage, FrontPageImage
 from django.conf import settings
 from django.http import HttpResponse
